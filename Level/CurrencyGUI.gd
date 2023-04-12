f6ec74bbab5b486d94ba746e8910d924ca259e0c
Aditha Doratiyawa <indegamedev@gmail.com>
2023-04-12 16:39:01 +1000

    added basic currency system

---
+++ Level/CurrencyGUI.gd
@@ -0,0 +1,15 @@
+extends Label
+
+@export var currency_manager : Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	currency_manager.currency_changed.connect(update_currency)
+	pass # Replace with function body.
+
+func update_currency(new_value):
+	text = str(new_value)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
