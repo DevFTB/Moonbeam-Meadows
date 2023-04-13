3d3d76cafe449b1a6eb263cf85b46dd489d1e1db
Aditha Doratiyawa <indegamedev@gmail.com>
2023-04-13 19:17:00 +1000

    added support for buy fert. and robots

--- Level/Trade Station/TradeStationGUI.gd
+++ Level/Trade Station/TradeStationGUI.gd
@@ -1,9 +1,11 @@
 extends Panel
 
-var crop_buy_gui_scene = preload("res://Level/Trade Station/ts_buy_item_gui.tscn")
-var crop_sell_gui_scene = preload("res://Level/Trade Station/ts_sell_item_gui.tscn")
+var buy_gui_scene = preload("res://Level/Trade Station/ts_buy_item_gui.tscn")
+var sell_gui_scene = preload("res://Level/Trade Station/ts_sell_item_gui.tscn")
 
 @export var buy_seed_parent : Control
+@export var buy_fertiliser_parent : Control
+@export var buy_robots_parent : Control
 @export var sell_seed_parent : Control
 
 @onready var level = get_node("/root/Level")
@@ -12,19 +14,21 @@ var trade_station : Node = null
 
 func _ready():
 	visibility_changed.connect(update_gui)
-	for seed_item in level.crops.map(func(c): return c.seed_item):
-		var buy_item_gui = crop_buy_gui_scene.instantiate()
-		buy_seed_parent.add_child(buy_item_gui)
-		buy_item_gui.set_item(seed_item)
-	
-	for produce_item in level.crops.map(func(c): return c.produce_item):
-		var sell_item_gui = crop_sell_gui_scene.instantiate()
-		sell_seed_parent.add_child(sell_item_gui)
-		sell_item_gui.set_item(produce_item)
+	generate_items(buy_seed_parent, buy_gui_scene, level.crops.map(func(c): return c.seed_item))
+	generate_items(buy_fertiliser_parent, buy_gui_scene, level.fertilisers.map(func(c): return c.fertiliser_item))
+#	generate_items(buy_robots_parent, buy_gui_scene, level.crops.map(func(c): return c.seed_item))
+	generate_items(sell_seed_parent, sell_gui_scene, level.crops.map(func(c): return c.produce_item))
+
+func generate_items(parent: Control, item_scene: PackedScene, items: Array):
+	for item in items:
+		var item_gui = item_scene.instantiate()
+		parent.add_child(item_gui)
+		item_gui.set_item(item)
 
 func update_gui():
-	for child in buy_seed_parent.get_children():
-		child.update_gui()
+	for parent in [buy_seed_parent, buy_fertiliser_parent, buy_robots_parent, sell_seed_parent]:
+		for child in parent.get_children():
+			child.update_gui()
 
 func set_trade_station(new_trade_station: Node):
 	trade_station = new_trade_station
